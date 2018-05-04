9b2fe169382ea950355e549de45fc944b778f599
kuroshibin <hahahathai@gmail.com>
2018-05-04 22:23:16 +0700

    Update installer-sendmessage.sh

--- installer-sendmessage.sh
+++ installer-sendmessage.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 apt-get update
-pkg install unzip ruby boxes fonts-tlwg-garuda wget curl figlet toilet -y
-cd $HOME
+pkg install unzip
+pkg install ruby
+pkg install boxes
+pkg install fonts-tlwg-garuda
 pkg install wget
+pkg install curl
+pkg install figlet
+pkg install toilet -y
+cd $HOME
 wget https://github.com/busyloop/lolcat/archive/master.zip
 unzip -f master.zip
 cd lolcat-master/bin
